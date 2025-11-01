e7240cbe06d00e8151d5aece4330118be7c68a00
rizkyputra0052 <rizkyputra0052@gmail.com>
2025-11-02 02:13:21 +0700

    Jawaban UTS NO 4

--- resources/views/tambah-produk.blade.php
+++ resources/views/tambah-produk.blade.php
@@ -1,33 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="mb-4">Form Tambah Produk</h1>
+<div class="container mt-4">
+    <h1 class="mb-4">Form Tambah Produk</h1>
 
-<div class="card">
-    <div class="card-body">
-        <form>
-            <div class="mb-3">
+    <form>
+        <!-- Baris Pertama -->
+        <div class="row g-3">
+            <div class="col-md-4">
                 <label for="kode" class="form-label">Kode Produk</label>
-                <input type="text" class="form-control" id="kode" name="kode">
+                <input type="text" class="form-control" id="kode" name="kode" placeholder="Input Kode Produk">
             </div>
             
-            <div class="mb-3">
+            <div class="col-md-4">
                 <label for="nama" class="form-label">Nama Produk</label>
-                <input type="text" class="form-control" id="nama" name="nama">
+                <input type="text" class="form-control" id="nama" name="nama" placeholder="Input Nama Produk">
             </div>
             
-            <div class="mb-3">
+            <div class="col-md-4">
                 <label for="jenis" class="form-label">Jenis Produk</label>
-                <input type="text" class="form-control" id="jenis" name="jenis">
+                <select class="form-select" id="jenis" name="jenis">
+                    <option selected>Pilih Produk</option>
+                    <option value="Alat Tulis">Alat Tulis</option>
+                    <option value="Elektronik">Elektronik</option>
+                    <option value="Lainnya">Lainnya</option>
+                </select>
+            </div>
+        </div>
+
+        <!-- Baris Kedua -->
+        <div class="row g-3 mt-2">
+            <div class="col-md-8">
+                <label for="harga" class="form-label">Harga</label>
+                <input type="number" class="form-control" id="harga" name="harga" placeholder="Input Harga">
             </div>
             
-            <div class="mb-3">
-                <label for="harga" class="form-label">Harga</label>
-                <input type="number" class="form-control" id="harga" name="harga">
+            <div class="col-md-4 d-flex align-items-end">
+                <button type="submit" class="btn btn-success w-100">Simpan</button>
             </div>
-
-            <button type="submit" class="btn btn-primary">Simpan</button>
-        </form>
-    </div>
+        </div>
+    </form>
 </div>
 @endsection
